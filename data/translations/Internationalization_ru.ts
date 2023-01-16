4af2959c56abe3934e4c2f843dca4d8b220eff53
Dmytro <global@marketkernel.com>
2023-01-16 23:48:44 +0200

    etc

--- data/translations/Internationalization_ru.ts
+++ data/translations/Internationalization_ru.ts
@@ -1252,17 +1252,17 @@ Please solve them manually in the configuration file.</source>
         <source>Imgur Application Client ID</source>
         <translation>Ключ API Imgur</translation>
     </message>
-	<message>
+    <message>
         <location filename="../../src/config/generalconf.cpp" line="564"/>
         <source>Download.RU settings</source>
         <translation>Интеграция с Download.RU</translation>
     </message>
-	<message>
-	    <location filename="../../src/config/generalconf.cpp" line="572"/>
+    <message>
+        <location filename="../../src/config/generalconf.cpp" line="572"/>
         <source>Share image after upload</source>
         <translation>Сделать ссылку общедоступной после загрузки файла</translation>
     </message>
-	<message>
+    <message>
         <location filename="../../src/config/generalconf.cpp" line="574"/>
         <source>Share image after upload after uploading was successful</source>
         <translation>После загрузки файла просматривать смогут все, у кого есть ссылка</translation>
